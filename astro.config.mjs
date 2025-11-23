17836cd524026be0688f524b16d56d643f05c4ff
Stan <stan@email>
2025-11-23 13:20:56 +0100

    added script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,3 +4,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
 });
+
+export default defineConfig({
+  site: 'https://camundaflow.de',   // ← ovo dodaj/obavezno postavi tačno ovako
+  base: '/',                        // ← ako već imaš base, ostavi ovako
+  // ostalo ostaje isto
+});
