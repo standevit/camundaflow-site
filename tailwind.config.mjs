4fb8f549d71560b5290019846fb441a3bc8cba7b
Stan <stan@email>
2025-11-23 14:06:25 +0100

    Final working version with Layout + Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,11 @@
-import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./public/**/*.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
